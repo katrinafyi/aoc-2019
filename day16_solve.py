1df02a5c1e678501ffe33da1bf932a26497a27f5
Kenton Lam <kenton_lam@outlook.com>
2019-12-16 17:28:48 +1000

    garbage.

--- day16_solve.py
+++ day16_solve.py
@@ -42,40 +42,81 @@ def pattern(n):
 
 @lru_cache()
 def pattern2(n):
-    mat = scipy.sparse.dok_matrix((n, n))
+    mat_rows = []
 
     base = [0, 1, 0, -1]
     for row in range(n):
         if row % 100 == 0: print(row)
         this_base_len = (row+1) * len(base)
         start = row
-        for col in range(start, n):
-            this_index = (col+1) % this_base_len // (row+1)
-            if base[this_index]: mat[row, col] = base[this_index]
-            # print((row, col), base[this_index], end=' ')
-            pass
+        mat_rows.append([base[(col+1) % this_base_len // (row+1)] for col in range(start, n)])
+        # for col in range(start, n):
+        #     this_index = (col+1) % this_base_len // (row+1)
+        #     if base[this_index]: mat[row, col] = base[this_index]
+        #     # print((row, col), base[this_index], end=' ')
+        #     pass
+    # mat = scipy.sparse.lil_matrix((n, n))
     print('done dok construction')
-    return scipy.sparse.csr_matrix(mat)
+    return scipy.sparse.csr_matrix(mat_rows)
+
+def apply_pattern(vec, n):
+    out = 0
+    start = n
+    period = 4 * (n+1)
+    for i in range(start, len(vec), period):
+        for j in range(n+1):
+            if i+j >= len(vec): break
+            out += vec[i+j]
+    for i in range(start + period // 2, len(vec), period):
+        for j in range(n+1):
+            if i+j >= len(vec): break
+            out -= vec[i+j]
+    return out
 
 def solve_1(data):
     print(data)
+    data *= 2
 
-    data *= 10000
-    vec = np.array(data)
-    print('done array()')
-    print(len(data))
-    print(pattern2(len(data)))
-    x = vec
-    input('wait ')
-    for i in range(100):
-        print(i)
+    x = list(data)
+    print(len(x))
+    y = [None] * len(x)
+    suffix = [0] * len(x)
+    seen = set()
+    for iteration in range(10000):
+        # print(i)
         # print(x)
-        x = np.mod(np.abs(pattern(len(data)) @ x.T), 10)
-        # print(x)
+        for i in range(len(x)-1, -1, -1):
+            if i + 1 < len(x):
+                suffix[i] = suffix[i+1] + x[i]
+            else:
+                suffix[i] = x[i]
+
+        for i in range(len(x)-1, -1, -1):
+            # print(i)
+            # print(x)
+            # print(y)
+            if i + 2*(i+1) >= len(x):
+                y[i] = suffix[i]
+                if i+i+1 < len(x): y[i] -= suffix[2*i+1]
+            else:
+                # print(i, 'manual')
+                y[i] = apply_pattern(x, i)
+        
+        # correct = ((np.mod(np.abs(pattern(len(x)) @ np.array(x)), 10)))
+
+        for i, val in enumerate(y):
+            x[i] = abs(y[i]) % 10
+        print(i, x)
+        xtup = tuple(x[:8])
+        if xtup in seen: 
+            print(iteration, 'seen')
+            break
+        seen.add(xtup)
+        # for i, val in enumerate(correct - x):
+        #     if val:
+        #         print(i, pattern(len(x))[i,:])
         # input()
-    print(x)
-    print(tuple(x)[:8])
-
+    print(x[:8])
     # pattern = 
 
 
